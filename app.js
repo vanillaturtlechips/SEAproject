0620ad8439ccdf03c4403cef5839a6f3e6e2ba5a
vanillaturtlechips <2220110150@office.kopo.ac.kr>
2025-04-30 16:09:11 +0900

    제발

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end('hello world argocd!\n'); // 메시지 변경
 });
 
 const port = process.env.PORT || 3000;
